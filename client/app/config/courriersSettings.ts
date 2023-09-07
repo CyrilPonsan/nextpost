29c66d509bb5e9ac1de16a270a87cba667b5a903
CyrilPonsan <cyril.ponsan@gmail.com>
2023-09-07 07:30:14 +0200

    reset

--- client/app/config/courriersSettings.ts
+++
@@ -1,7 +0,0 @@
-export const defaultLimit = "5";
-
-export const defaultSortedField = "bordereau";
-
-export const routeCourrierFields = ["type", "bordereau", "nom", "etat", "date"];
-
-export const defaultDirection = "DESC";
